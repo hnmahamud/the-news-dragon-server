4da46051ff275d1593da23648a03159f5df4b60f
hnmahamud <hnmahamud@gmail.com>
2023-04-25 02:32:21 +0600

    make api for all news, single news, single category

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Dragon is running.......................");
+  res.send("Dragon is running.....");
 });
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 8) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter(
+      (singleNews) => parseInt(singleNews.category_id) === id
+    );
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const specificNews = news.find((singleNews) => singleNews._id === id);
+  res.send(specificNews);
 });
 
 app.listen(port, () => {
